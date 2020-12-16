2712e526fe8b3f720cef9a806946be363fbca865
yuliyalop <penguindobro@gmail.com>
2020-12-16 15:17:57 +0300

    project create

---
+++ Runar/Runar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Runar
+//
+//  Created by Юлия Лопатина on 16.12.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
