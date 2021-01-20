ac0a850263d843a6dad64965c87fe2b892947155
Oleg Kanatov <olegkanatov7@gmail.com>
2021-01-20 14:57:40 +0300

    add Assets, Fonts, Localizable

---
+++ Runar/Runar/SwiftGen/Assets.swift
@@ -0,0 +1,137 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+#if os(macOS)
+  import AppKit
+#elseif os(iOS)
+  import UIKit
+#elseif os(tvOS) || os(watchOS)
+  import UIKit
+#endif
+
+// Deprecated typealiases
+@available(*, deprecated, renamed: "ColorAsset.Color", message: "This typealias will be removed in SwiftGen 7.0")
+internal typealias AssetColorTypeAlias = ColorAsset.Color
+@available(*, deprecated, renamed: "ImageAsset.Image", message: "This typealias will be removed in SwiftGen 7.0")
+internal typealias AssetImageTypeAlias = ImageAsset.Image
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return
+
+// MARK: - Asset Catalogs
+
+// swiftlint:disable identifier_name line_length nesting type_body_length type_name
+internal enum Assets {
+  internal static let accentColor = ColorAsset(name: "AccentColor")
+  internal static let кельтскийКрест = ImageAsset(name: "Кельтский крест")
+  internal static let краткийПрогноз = ImageAsset(name: "Краткий прогноз")
+  internal static let крестСтихий = ImageAsset(name: "Крест стихий")
+  internal static let крест = ImageAsset(name: "Крест")
+  internal static let молотТора = ImageAsset(name: "Молот Тора")
+  internal static let норны = ImageAsset(name: "Норны")
+  internal static let раскладИз2Рун = ImageAsset(name: "Расклад из 2 рун")
+  internal static let рунаДня = ImageAsset(name: "Руна дня")
+  internal static let backgroundDown = ImageAsset(name: "backgroundDown")
+  internal static let bookmark = ImageAsset(name: "bookmark")
+  internal static let escape = ImageAsset(name: "escape")
+  internal static let favourite = ImageAsset(name: "favourite")
+  internal static let floatingDown = ImageAsset(name: "floatingDown")
+  internal static let floatingUp = ImageAsset(name: "floatingUp")
+  internal static let home = ImageAsset(name: "home")
+  internal static let main = ImageAsset(name: "main")
+  internal static let mainFire = ImageAsset(name: "main_fire")
+  internal static let person2 = ImageAsset(name: "person 2")
+  internal static let selected = ImageAsset(name: "selected")
+  internal static let unselected = ImageAsset(name: "unselected")
+}
+// swiftlint:enable identifier_name line_length nesting type_body_length type_name
+
+// MARK: - Implementation Details
+
+internal final class ColorAsset {
+  internal fileprivate(set) var name: String
+
+  #if os(macOS)
+  internal typealias Color = NSColor
+  #elseif os(iOS) || os(tvOS) || os(watchOS)
+  internal typealias Color = UIColor
+  #endif
+
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
+  internal private(set) lazy var color: Color = {
+    guard let color = Color(asset: self) else {
+      fatalError("Unable to load color asset named \(name).")
+    }
+    return color
+  }()
+
+  fileprivate init(name: String) {
+    self.name = name
+  }
+}
+
+internal extension ColorAsset.Color {
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
+  convenience init?(asset: ColorAsset) {
+    let bundle = BundleToken.bundle
+    #if os(iOS) || os(tvOS)
+    self.init(named: asset.name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    self.init(named: NSColor.Name(asset.name), bundle: bundle)
+    #elseif os(watchOS)
+    self.init(named: asset.name)
+    #endif
+  }
+}
+
+internal struct ImageAsset {
+  internal fileprivate(set) var name: String
+
+  #if os(macOS)
+  internal typealias Image = NSImage
+  #elseif os(iOS) || os(tvOS) || os(watchOS)
+  internal typealias Image = UIImage
+  #endif
+
+  internal var image: Image {
+    let bundle = BundleToken.bundle
+    #if os(iOS) || os(tvOS)
+    let image = Image(named: name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    let name = NSImage.Name(self.name)
+    let image = (bundle == .main) ? NSImage(named: name) : bundle.image(forResource: name)
+    #elseif os(watchOS)
+    let image = Image(named: name)
+    #endif
+    guard let result = image else {
+      fatalError("Unable to load image asset named \(name).")
+    }
+    return result
+  }
+}
+
+internal extension ImageAsset.Image {
+  @available(macOS, deprecated,
+    message: "This initializer is unsafe on macOS, please use the ImageAsset.image property")
+  convenience init?(asset: ImageAsset) {
+    #if os(iOS) || os(tvOS)
+    let bundle = BundleToken.bundle
+    self.init(named: asset.name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    self.init(named: NSImage.Name(asset.name))
+    #elseif os(watchOS)
+    self.init(named: asset.name)
+    #endif
+  }
+}
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type
