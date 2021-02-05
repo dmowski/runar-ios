ab020709f11d2adae720abf000b3f659fd555d86
yuliyalop <penguindobro@gmail.com>
2021-02-05 15:30:43 +0300

    Fixed: slots wrong places

--- Runar/Runar/Support/DataBase.swift
+++ Runar/Runar/Support/DataBase.swift
@@ -13,8 +13,8 @@ struct DataBase {
         RuneDescription(id: UUID().uuidString, name: L10n.Rune.DayRune.name, layout: .dayRune, description: L10n.Rune.DayRune.description),
         RuneDescription(id: UUID().uuidString, name: L10n.Rune.TwoRunes.name, layout: .twoRunes, description: L10n.Rune.TwoRunes.description),
         RuneDescription(id: UUID().uuidString, name: L10n.Rune.Norns.name, layout: .norns, description: L10n.Rune.Norns.description),
-        RuneDescription(id: UUID().uuidString, name: L10n.Rune.ThorsHummer.name, layout: .thorsHummer, description: L10n.Rune.ThorsHummer.description),
         RuneDescription(id: UUID().uuidString, name: L10n.Rune.ShortPrediction.name, layout: .shortPrediction, description: L10n.Rune.ShortPrediction.description),
+        RuneDescription(id: UUID().uuidString, name: L10n.Rune.ThorsHummer.name, layout: .thorsHummer, description: L10n.Rune.ThorsHummer.description),
         RuneDescription(id: UUID().uuidString, name: L10n.Rune.Cross.name, layout: .cross, description: L10n.Rune.Cross.description),
         RuneDescription(id: UUID().uuidString, name: L10n.Rune.ElementsCross.name, layout: .elementsCross, description: L10n.Rune.ElementsCross.description),
         RuneDescription(id: UUID().uuidString, name: L10n.Rune.KeltsCross.name, layout: .keltsCross, description: L10n.Rune.KeltsCross.description)
