5e223619af95f28bed10f64ef91859482cdb8539
Oleg Kanatov <olegkanatov7@gmail.com>
2021-03-11 02:49:02 +0300

    add blur, fix UI-elements, delete Storyboard

--- Runar/Runar/SwiftGen/Assets.swift
+++ Runar/Runar/SwiftGen/Assets.swift
@@ -22,6 +22,28 @@ internal typealias AssetImageTypeAlias = ImageAsset.Image
 // swiftlint:disable identifier_name line_length nesting type_body_length type_name
 internal enum Assets {
   internal static let accentColor = ColorAsset(name: "AccentColor")
+  internal enum Background {
+    internal static let main = ImageAsset(name: "Background/main")
+  }
+  internal enum CellSettings {
+    internal enum Colors {
+      internal static let backgroundColor = ColorAsset(name: "CellSettings/Colors/backgroundColor")
+      internal static let backgroundColorHighlite = ColorAsset(name: "CellSettings/Colors/backgroundColorHighlite")
+      internal static let borderColor = ColorAsset(name: "CellSettings/Colors/borderColor")
+      internal static let colorDidLight = ColorAsset(name: "CellSettings/Colors/colorDidLight")
+      internal static let colorLight = ColorAsset(name: "CellSettings/Colors/colorLight")
+    }
+    internal enum Runes {
+      internal static let cross = ImageAsset(name: "CellSettings/Runes/cross")
+      internal static let dayRune = ImageAsset(name: "CellSettings/Runes/dayRune")
+      internal static let elementsCross = ImageAsset(name: "CellSettings/Runes/elementsCross")
+      internal static let keltsCross = ImageAsset(name: "CellSettings/Runes/keltsCross")
+      internal static let norns = ImageAsset(name: "CellSettings/Runes/norns")
+      internal static let shortPrediction = ImageAsset(name: "CellSettings/Runes/shortPrediction")
+      internal static let thorsHummer = ImageAsset(name: "CellSettings/Runes/thorsHummer")
+      internal static let twoRunes = ImageAsset(name: "CellSettings/Runes/twoRunes")
+    }
+  }
   internal enum Colors {
     internal static let textColor = ColorAsset(name: "Colors/TextColor")
     internal enum Touch {
@@ -35,10 +57,6 @@ internal enum Assets {
       internal static let text = ColorAsset(name: "Colors/UnTouch/text")
     }
   }
-  internal static let favouriteSelected = ImageAsset(name: "Favourite-selected")
-  internal static let favouriteUnselected = ImageAsset(name: "Favourite-unselected")
-  internal static let homeSelected = ImageAsset(name: "Home-selected")
-  internal static let homeUnselected = ImageAsset(name: "Home-unselected")
   internal static let interpretationBackground = ImageAsset(name: "InterpretationBackground")
   internal enum LayoutsRunes {
     internal static let algiz = ImageAsset(name: "LayoutsRunes/Algiz")
@@ -85,24 +103,20 @@ internal enum Assets {
     internal static let uruz = ImageAsset(name: "LayoutsRunes/Uruz")
     internal static let wunji = ImageAsset(name: "LayoutsRunes/Wunji")
   }
-  internal static let librarySelected = ImageAsset(name: "Library-selected")
-  internal static let library = ImageAsset(name: "Library")
-  internal static let cross = ImageAsset(name: "cross")
-  internal static let dayRune = ImageAsset(name: "dayRune")
-  internal static let elementsCross = ImageAsset(name: "elementsCross")
-  internal static let keltsCross = ImageAsset(name: "keltsCross")
-  internal static let norns = ImageAsset(name: "norns")
-  internal static let shortPrediction = ImageAsset(name: "shortPrediction")
-  internal static let thorsHummer = ImageAsset(name: "thorsHummer")
-  internal static let twoRunes = ImageAsset(name: "twoRunes")
-  internal static let settingsSelected = ImageAsset(name: "Settings-selected")
-  internal static let settingsUnselected = ImageAsset(name: "Settings-unselected")
+  internal enum TabBar {
+    internal enum Image {
+      internal static let favorites = ImageAsset(name: "TabBar/Image/favorites")
+      internal static let home = ImageAsset(name: "TabBar/Image/home")
+      internal static let library = ImageAsset(name: "TabBar/Image/library")
+      internal static let settings = ImageAsset(name: "TabBar/Image/settings")
+    }
+    internal static let pushColor = ColorAsset(name: "TabBar/pushColor")
+  }
   internal static let ads = ImageAsset(name: "ads")
   internal static let backgroundDown = ImageAsset(name: "backgroundDown")
   internal static let escape = ImageAsset(name: "escape")
   internal static let floatingDown = ImageAsset(name: "floatingDown")
   internal static let floatingUp = ImageAsset(name: "floatingUp")
-  internal static let main = ImageAsset(name: "main")
   internal static let mainFire = ImageAsset(name: "main_fire")
   internal static let person2 = ImageAsset(name: "person 2")
   internal static let selected = ImageAsset(name: "selected")
--- Runar/Runar/Views/Cells/MainCell.swift
+++ Runar/Runar/Views/Cells/MainCell.swift
@@ -13,26 +13,23 @@ class MainCell: UICollectionViewCell {
     private static let fontMultiplier : CGFloat = 32/199
     private static let labelBottomMultiplier : CGFloat = 12/199
     private static let labelTopMultiplier: CGFloat = 156/199
-    private static let imageLeftMultiplier: CGFloat = 59/183
+    private static let imageLeftMultiplier: CGFloat = 45/183
     var image = UIImageView()
-    
     
     override var isHighlighted: Bool {
         didSet {
             if self.isHighlighted {
-                contentView.layer.backgroundColor = UIColor(red: 1, green: 0.918, blue: 0.792, alpha: 0.15).cgColor
-                contentView.layer.borderColor = UIColor(red: 0.937, green: 0.804, blue: 0.576, alpha: 1).cgColor
+                contentView.layer.backgroundColor = Assets.CellSettings.Colors.backgroundColorHighlite.color.cgColor
+                contentView.layer.borderColor = Assets.CellSettings.Colors.colorLight.color.cgColor
             } else {
-                contentView.layer.backgroundColor = UIColor(red: 0.063, green: 0.063, blue: 0.063, alpha: 0.35).cgColor
-                contentView.layer.borderColor = UIColor(red: 0.824, green: 0.769, blue: 0.678, alpha: 0.7).cgColor
-
-                
+                contentView.layer.backgroundColor = Assets.CellSettings.Colors.backgroundColor.color.cgColor
+                contentView.layer.borderColor = Assets.CellSettings.Colors.borderColor.color.cgColor
             }
         }
     }
     
     private let alignmentLabel = UILabel()
-   
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setUpCell()
@@ -47,12 +44,10 @@ class MainCell: UICollectionViewCell {
         contentView.layer.cornerRadius = 8
         contentView.layer.borderWidth = 1
         contentView.layer.masksToBounds = true
-        contentView.layer.borderColor = UIColor(red: 0.824, green: 0.769, blue: 0.678, alpha: 0.7).cgColor
-
-        contentView.layer.backgroundColor = UIColor(red: 0.146, green: 0.146, blue: 0.146, alpha: 0.35).cgColor
+        contentView.layer.borderColor = Assets.CellSettings.Colors.borderColor.color.cgColor
+        contentView.layer.backgroundColor = Assets.CellSettings.Colors.backgroundColor.color.cgColor
         
     }
-
     
     func update(with model: MainCellModel?) {
         guard let model = model else { return }
@@ -63,10 +58,10 @@ class MainCell: UICollectionViewCell {
     func setUpRune() {
         alignmentLabel.translatesAutoresizingMaskIntoConstraints = false
         image.translatesAutoresizingMaskIntoConstraints = false
-        alignmentLabel.textColor =  UIColor(red: 0.825, green: 0.77, blue: 0.677, alpha: 1)
+        alignmentLabel.textColor = Assets.CellSettings.Colors.colorDidLight.color
         alignmentLabel.textAlignment = .center
-        alignmentLabel.highlightedTextColor = UIColor(red: 0.988, green: 0.784, blue: 0.455, alpha: 1)
-        alignmentLabel.font = UIFont(name: "AmaticSC-Bold", size: self.frame.size.height * MainCell.fontMultiplier)
+        alignmentLabel.highlightedTextColor = Assets.CellSettings.Colors.colorLight.color
+        alignmentLabel.font = UIFont(name: FontFamily.AmaticSC.bold.family, size: self.frame.size.height * MainCell.fontMultiplier)
         contentView.addSubview(alignmentLabel)
         contentView.addSubview(image)
         
@@ -74,37 +69,13 @@ class MainCell: UICollectionViewCell {
             alignmentLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 9),
             alignmentLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -9),
             alignmentLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -(self.frame.size.height * MainCell.labelBottomMultiplier)),
-                        alignmentLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: (self.frame.size.height * MainCell.labelTopMultiplier)),
+            alignmentLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: (self.frame.size.height * MainCell.labelTopMultiplier)),
             
-            image.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 12),
+            image.topAnchor.constraint(equalTo: contentView.topAnchor),
             image.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: (self.frame.size.width * MainCell.imageLeftMultiplier)),
-                         image.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            image.bottomAnchor.constraint(equalTo: alignmentLabel.topAnchor, constant: -13)
+            image.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            image.bottomAnchor.constraint(equalTo: alignmentLabel.topAnchor, constant: -1)
         ])
-        
     }
 }
 
-
-private extension RuneDescription {
-    var image: UIImage {
-        switch layout {
-        case .dayRune:
-            return Assets.dayRune.image
-        case .twoRunes:
-            return Assets.twoRunes.image
-        case .norns:
-            return Assets.norns.image
-        case .thorsHummer:
-            return Assets.thorsHummer.image
-        case .shortPrediction:
-            return Assets.shortPrediction.image
-        case .cross:
-            return Assets.cross.image
-        case .elementsCross:
-            return Assets.elementsCross.image
-        case .keltsCross:
-            return Assets.keltsCross.image
-        }
-    }
-}
