f3003319cc34dcfa92a7131c6214c9e00ed199d4
Oleg Kanatov <olegkanatov7@gmail.com>
2021-03-15 16:23:17 +0300

    add scrollView and text description

--- Runar/Runar/Controllers/OneRuneViewController/OneRuneViewController.swift
+++ Runar/Runar/Controllers/OneRuneViewController/OneRuneViewController.swift
@@ -12,11 +12,15 @@ class OneRuneViewController: UIViewController {
     private var runeType : RuneType?
     public var runesSet = [RuneType]()
     private var runeTime = String()
+    var topLine : TopLineView?
+    var bottomLine : BottomLineView?
     
     init(runeType: RuneType, runeTime: String) {
         super.init(nibName: nil, bundle: nil)
         self.runeType = runeType
         self.runeTime = runeTime
+        topLine = TopLineView(runeType: runeType , runeTime: runeTime)
+        bottomLine = BottomLineView(runesSet: runesSet)
     }
     
     required init?(coder: NSCoder) {
@@ -28,6 +32,8 @@ class OneRuneViewController: UIViewController {
         setView()
         setUpTopLineConstr()
         setUpBottomConstr()
+        setUpScrollViewDescription()
+        setUpDescriptionLabel()
     }
     
     private func setView() {
@@ -40,11 +46,11 @@ class OneRuneViewController: UIViewController {
         stroke.layer.borderWidth = 1
         stroke.layer.borderColor = UIColor(red: 0.329, green: 0.329, blue: 0.345, alpha: 0.65).cgColor
         stroke.layer.cornerRadius = 20
-
+        
     }
     
     private func setUpTopLineConstr() {
-        let topLine = TopLineView(runeType: runeType ?? RuneType.sowilu, runeTime: runeTime)
+        guard let topLine = topLine else {return}
         view.addSubview(topLine)
         NSLayoutConstraint.activate([
             topLine.topAnchor.constraint(equalTo: view.topAnchor),
@@ -55,16 +61,72 @@ class OneRuneViewController: UIViewController {
     }
     
     private func setUpBottomConstr() {
-        let bottom = BottomLineView(runesSet: self.runesSet)
-        bottom.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(bottom)
+        guard let bottomLine = bottomLine else {return}
+        bottomLine.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bottomLine)
         NSLayoutConstraint.activate([
-            bottom.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            bottom.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            bottom.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            bottom.heightAnchor.constraint(equalToConstant: 95.heightDependent())
+            bottomLine.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            bottomLine.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            bottomLine.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            bottomLine.heightAnchor.constraint(equalToConstant: 95.heightDependent())
         ])
     }
-
-
+    
+    //-------------------------------------------------
+    // MARK: - ScrollViewDescription
+    //-------------------------------------------------
+    
+    private var scrollViewDescription = UIScrollView()
+    private let contentLabel = UILabel()
+    
+    private func setUpScrollViewDescription() {
+        guard let topLine = topLine else {return}
+        guard let bottomLine = bottomLine else {return}
+        
+        scrollViewDescription.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(scrollViewDescription)
+        
+        NSLayoutConstraint.activate([
+            scrollViewDescription.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            scrollViewDescription.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            scrollViewDescription.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            scrollViewDescription.topAnchor.constraint(equalTo: topLine.bottomAnchor),
+            scrollViewDescription.bottomAnchor.constraint(equalTo: bottomLine.topAnchor),
+        ])
+    }
+    
+    //-------------------------------------------------
+    // MARK: - DescriptionLabel
+    //-------------------------------------------------
+    
+    private func setUpDescriptionLabel() {
+        let descriptionLabel: UILabel = {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.numberOfLines = 0
+            label.sizeToFit()
+            return label
+        }()
+        
+        guard let description = runeType?.description else {return}
+       
+        let timeParagraphStyle = NSMutableParagraphStyle()
+        timeParagraphStyle.lineHeightMultiple = 1.23
+        
+        let atributes: [NSAttributedString.Key: Any] = [
+            .font: FontFamily.SFProDisplay.light.font(size: 19),
+            .foregroundColor: UIColor(red: 0.855, green: 0.855, blue: 0.855, alpha: 1),
+            .paragraphStyle: timeParagraphStyle,
+            .kern: -0.38,
+        ]
+        descriptionLabel.attributedText = NSMutableAttributedString(string: description, attributes: atributes)
+    
+        scrollViewDescription.addSubview(descriptionLabel)
+        NSLayoutConstraint.activate([
+            descriptionLabel.topAnchor.constraint(equalTo: scrollViewDescription.topAnchor),
+            descriptionLabel.widthAnchor.constraint(equalTo: scrollViewDescription.widthAnchor),
+            descriptionLabel.bottomAnchor.constraint(equalTo: scrollViewDescription.bottomAnchor)
+        ])
+    }
 }
