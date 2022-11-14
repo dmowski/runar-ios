a53cb487e23085bface2eb56b438a496fbbbed40
Anton Mishkevich <mian19@protonmail.com>
2022-11-14 16:28:50 +0300

    fix: move extension for customColors from SettingsVC, LibraryViewController to separate file; changed the color names

---
+++ Runar/Runar/Extensions/UIColor+customColors.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+customColors.swift
+//  Runar
+//
+//  Created by Kyzu on 14.11.22.
+//
+
+import UIKit
+
+public extension UIColor {
+    static let titleColor = UIColor(red: 0.937, green: 0.804, blue: 0.576, alpha: 1)
+    static let navBarBackgroundColor = UIColor(red: 0.092, green: 0.092, blue: 0.092, alpha: 0.94)
+}
