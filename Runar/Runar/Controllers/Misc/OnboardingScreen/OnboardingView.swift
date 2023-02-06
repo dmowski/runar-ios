11ce8e8a77f518c30dcf8392ed86fa6925e44fe4
Kengii <kengii9431@gmail.com>
2023-02-06 12:49:19 +0300

    fix comment

--- Runar/Runar/Controllers/Misc/OnboardingScreen/OnboardingView.swift
+++ Runar/Runar/Controllers/Misc/OnboardingScreen/OnboardingView.swift
@@ -23,10 +23,6 @@ class OnboardingView: UIView {
     let pageControll = CustomPageControll()
     
     // MARK: Constants
-    let factorConstant: CGFloat = DeviceType.iPhoneSE ? 0.135 : 0.207
-    let heightButton: CGFloat = 48
-    let widthButton: CGFloat = 264
-    // this is a constant, a multiplier that is calculated from the screen dimensions, for the correct placement of the button on the screen
     
     weak var onboardingViewDelegate: OnboardingViewDelegateProtocol?
     
@@ -64,13 +60,13 @@ class OnboardingView: UIView {
     private func configureSkipButton() {
         onboardingSkipButton.backgroundColor = .clear
         onboardingSkipButton.setTitle(L10n.Onboarding.skip, for: .normal)
-        onboardingSkipButton.titleLabel?.font = UIFont(name: "SFProText-Regular", size: 17)
-        onboardingSkipButton.titleLabel?.tintColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.7)
+        onboardingSkipButton.titleLabel?.font = .systemRegular(size: CGFloat.fontSizeSkipButton)
+        onboardingSkipButton.titleLabel?.tintColor = UIColor.tintColorSkipButton
         addSubview(onboardingSkipButton)
         onboardingSkipButton.addTarget(self, action: #selector(goToMainScreen), for: .touchUpInside)
         onboardingSkipButton.snp.makeConstraints { make in
-            make.trailing.equalToSuperview().inset(24)
-            make.top.equalTo(safeAreaLayoutGuide).inset(20)
+            make.trailing.equalToSuperview().inset(CGFloat.trailingAnchorSkipButton)
+            make.top.equalTo(safeAreaLayoutGuide).inset(CGFloat.topAnchorSkipButton)
         }
     }
     
@@ -84,88 +80,83 @@ class OnboardingView: UIView {
         collectionView.showsHorizontalScrollIndicator = false
         addSubview(collectionView)
         collectionView.snp.makeConstraints { make in
-            if !DeviceType.iPhoneSE && !DeviceType.isIPhone678 {
-                make.top.equalTo(onboardingSkipButton.snp.bottom).offset(114)
-                make.height.equalTo(350)
+            make.top.equalTo(onboardingSkipButton.snp.bottom).offset(CGFloat.topAnchorCollectionView)
+            make.height.equalTo(CGFloat.heightCollectionView)
                 make.leading.trailing.equalToSuperview()
-            } else {
-                make.top.equalTo(onboardingSkipButton.snp.bottom).offset(50)
-                make.height.equalTo(350)
-                make.leading.trailing.equalToSuperview()
-            }
+            
         }
         self.onboardingCollectionView = collectionView
     }
     
     private func configureNextSlideButton() {
         
-        onboardingNextSlideButton.layer.cornerRadius = 8
-        onboardingNextSlideButton.layer.borderWidth = 1
-
+        onboardingNextSlideButton.layer.cornerRadius = CGFloat.cornerRadiusSlideButton
+        onboardingNextSlideButton.layer.borderWidth = CGFloat.borderWidthSlideButton
+        
         onboardingNextSlideButton.setTitle(L10n.Onboarding.nextScreen, for: .normal)
-        onboardingNextSlideButton.backgroundColor = UIColor(red: 0.417, green: 0.417, blue: 0.417, alpha: 0.36)
-        onboardingNextSlideButton.layer.borderColor = UIColor(red: 0.825, green: 0.77, blue: 0.677, alpha: 1).cgColor
+        onboardingNextSlideButton.backgroundColor = UIColor.darkButtomColor
+        onboardingNextSlideButton.layer.borderColor = UIColor.yellowPrimaryColor.cgColor
         onboardingNextSlideButton.translatesAutoresizingMaskIntoConstraints = false
 
-        onboardingNextSlideButton.titleLabel?.font = .amaticBold(size: fontSize)
+        onboardingNextSlideButton.titleLabel?.font = .amaticBold(size: CGFloat.fontSlideButton)
 
-        onboardingNextSlideButton.setTitleColor(UIColor(red: 0.825, green: 0.77, blue: 0.677, alpha: 1), for: .normal)
-        onboardingNextSlideButton.setTitleColor(UIColor(red: 0.294, green: 0.282, blue: 0.259, alpha: 1), for: .highlighted)
+        onboardingNextSlideButton.setTitleColor(UIColor.yellowPrimaryColor, for: .normal)
+        onboardingNextSlideButton.setTitleColor(UIColor.lightTitleButtomColor, for: .highlighted)
         addSubview(onboardingNextSlideButton)
         onboardingNextSlideButton.addTarget(self, action: #selector(goToNextSlide), for: .touchUpInside)
         onboardingNextSlideButton.addTarget(self, action: #selector(changeButtonColor(_:)), for: .touchDown)
         onboardingNextSlideButton.addTarget(self, action: #selector(buttonNormalColor(_:)), for: .touchUpInside)
         onboardingNextSlideButton.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
-            make.width.equalTo(widthButton)
-            make.height.equalTo(heightButton)
-            make.bottom.equalToSuperview().inset(ScreenSize.height * factorConstant)
+            make.width.equalTo(CGFloat.widthButton)
+            make.height.equalTo(CGFloat.heightButton)
+            make.bottom.equalToSuperview().inset(ScreenSize.height * CGFloat.bottomAnchorSlideButton)
         }
     }
     
     func configureOnboardingStartButton() {
         
         onboardingStartButton.isHidden = true
-        onboardingStartButton.layer.cornerRadius = 8
-        onboardingStartButton.layer.borderWidth = 1
+        onboardingStartButton.layer.cornerRadius = CGFloat.cornerRadiusSlideButton
+        onboardingStartButton.layer.borderWidth = CGFloat.borderWidthSlideButton
         onboardingStartButton.setTitle(L10n.Onboarding.start, for: .normal)
-        onboardingStartButton.backgroundColor = UIColor(red: 0.937, green: 0.804, blue: 0.576, alpha: 1)
-        onboardingStartButton.layer.borderColor = UIColor(red: 1, green: 0.917, blue: 0.792, alpha: 0.7).cgColor
+        onboardingStartButton.backgroundColor = UIColor.yellowSecondaryColor
+        onboardingStartButton.layer.borderColor = UIColor.lightBorderButtomColor.cgColor
         onboardingStartButton.translatesAutoresizingMaskIntoConstraints = false
-        onboardingStartButton.titleLabel?.font = FontFamily.AmaticSC.bold.font(size: 24)
-        onboardingStartButton.setTitleColor(UIColor(red: 0.825, green: 0.77, blue: 0.677, alpha: 1), for: .highlighted)
-        onboardingStartButton.setTitleColor(UIColor(red: 0.294, green: 0.282, blue: 0.259, alpha: 1), for: .normal)
+        
+        onboardingStartButton.titleLabel?.font = .amaticBold(size: CGFloat.fontSlideButton)
+        
+        onboardingStartButton.setTitleColor(UIColor.yellowPrimaryColor, for: .highlighted)
+        onboardingStartButton.setTitleColor(UIColor.lightTitleButtomColor, for: .normal)
         addSubview(onboardingStartButton)
         onboardingStartButton.addTarget(self, action: #selector(goToNextSlide), for: .touchUpInside)
         onboardingStartButton.addTarget(self, action: #selector(changeButtonColor(_:)), for: .touchDown)
         onboardingStartButton.addTarget(self, action: #selector(buttonNormalColor(_:)), for: .touchUpInside)
         onboardingStartButton.snp.makeConstraints { make in
-            print(ScreenSize.height)
             make.centerX.equalToSuperview()
-            make.width.equalTo(widthButton)
-            make.height.equalTo(heightButton)
-            make.bottom.equalToSuperview().inset(ScreenSize.height * factorConstant)
+            make.width.equalTo(CGFloat.widthButton)
+            make.height.equalTo(CGFloat.heightButton)
+            make.bottom.equalToSuperview().inset(ScreenSize.height * CGFloat.bottomAnchorSlideButton)
         }
     }
     
     func configureCustomPageControll() {
         pageControllStackView.axis = .horizontal
         pageControllStackView.distribution = .fillEqually
-        pageControllStackView.spacing = 8
+        pageControllStackView.spacing = CGFloat.spacingPageControlStackView
         addSubview(pageControllStackView)
         pageControllStackView.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
-            let constant = DeviceType.iPhoneSE ? 30 : 67
-                make.top.equalTo(onboardingNextSlideButton.snp.bottom).offset(constant)
+            make.top.equalTo(onboardingNextSlideButton.snp.bottom).offset(CGFloat.topAnchorPageControl)
         }
         pageControllStackView.removeFullyAllArrangedSubviews()
         pageControll.numberOfDots = OnboardingSlide.slides().count
         for number in 0..<pageControll.numberOfDots {
              let button = CustomPageControll()
             if pageControll.currentPage == number {
-                button.setUpIndicator(size: 8, indicatorColor: UIColor(red: 1, green: 0.753, blue: 0.275, alpha: 1), image: "circle.fill")  //Current page indicator
+                button.setUpIndicator(size: CGFloat.sizeIndicator, indicatorColor: UIColor.indicatorColor, image: "circle.fill")  //Current page indicator
              } else {
-                 button.setUpIndicator(size: 8, indicatorColor: UIColor(red: 1, green: 0.753, blue: 0.275, alpha: 1), image: "circle")  //Page indicator
+                 button.setUpIndicator(size: CGFloat.sizeIndicator, indicatorColor: UIColor.indicatorColor, image: "circle")  //Page indicator
              }
              pageControllStackView.addArrangedSubview(button)
          }
@@ -181,21 +172,21 @@ class OnboardingView: UIView {
     
     @objc private func buttonNormalColor(_ sender: UIButton) {
         if sender == onboardingStartButton {
-            onboardingStartButton.backgroundColor = UIColor(red: 0.937, green: 0.804, blue: 0.576, alpha: 1)
-            onboardingStartButton.layer.borderColor = UIColor(red: 1, green: 0.917, blue: 0.792, alpha: 0.7).cgColor
+            onboardingStartButton.backgroundColor = UIColor.yellowSecondaryColor
+            onboardingStartButton.layer.borderColor = UIColor.lightBorderButtomColor.cgColor
         } else {
-            onboardingNextSlideButton.backgroundColor = UIColor(red: 0.417, green: 0.417, blue: 0.417, alpha: 0.36)
-            onboardingNextSlideButton.layer.borderColor = UIColor(red: 0.825, green: 0.77, blue: 0.677, alpha: 1).cgColor
+            onboardingNextSlideButton.backgroundColor = UIColor.darkButtomColor
+            onboardingNextSlideButton.layer.borderColor = UIColor.yellowPrimaryColor.cgColor
         }
     }
     
     @objc private func changeButtonColor(_ sender: UIButton) {
         if sender == onboardingStartButton {
-            onboardingStartButton.backgroundColor = UIColor(red: 0.417, green: 0.417, blue: 0.417, alpha: 0.36)
-            onboardingStartButton.layer.borderColor = UIColor(red: 0.825, green: 0.77, blue: 0.677, alpha: 1).cgColor
+            onboardingStartButton.backgroundColor = UIColor.darkButtomColor
+            onboardingStartButton.layer.borderColor = UIColor.yellowPrimaryColor.cgColor
         } else {
-            onboardingNextSlideButton.backgroundColor = UIColor(red: 0.937, green: 0.804, blue: 0.576, alpha: 1)
-            onboardingNextSlideButton.layer.borderColor = UIColor(red: 1, green: 0.917, blue: 0.792, alpha: 0.7).cgColor
+            onboardingNextSlideButton.backgroundColor = UIColor.yellowSecondaryColor
+            onboardingNextSlideButton.layer.borderColor = UIColor.lightBorderButtomColor.cgColor
         }
     }
     
@@ -211,4 +202,33 @@ extension UIStackView {
             removeFully(view: view)
         }
     }
+}
+
+private extension CGFloat {
+    static let trailingAnchorSkipButton = 24.0
+    static let topAnchorSkipButton = 20.0
+    static let fontSizeSkipButton = 15.0
+    
+    static let topAnchorCollectionView = DeviceType.iPhoneSE || DeviceType.isIPhone678 ? 50.0 : 114.0
+    static let heightCollectionView = 350.0
+    
+    static let cornerRadiusSlideButton = 8.0
+    static let borderWidthSlideButton = 1.0
+    static let fontSlideButton: CGFloat = 24.0
+    /// multiplier calculated: the constraint (specified in the figure) to the bottom of the screen divided by the height of the screen
+    static let bottomAnchorSlideButton: CGFloat = DeviceType.iPhoneSE ? 0.135 : 0.2
+    static let heightButton: CGFloat = 48.0
+    static let widthButton: CGFloat = 264.0
+    
+    static let spacingPageControlStackView = 8.0
+    static let topAnchorPageControl = DeviceType.iPhoneSE ? 30 : 67
+    static let sizeIndicator = 8.0
+}
+
+private extension UIColor {
+    static let tintColorSkipButton = UIColor(red: 1, green: 1, blue: 1, alpha: 0.7)
+    static let darkButtomColor = UIColor(red: 0.417, green: 0.417, blue: 0.417, alpha: 0.36)
+    static let lightTitleButtomColor = UIColor(red: 0.294, green: 0.282, blue: 0.259, alpha: 1)
+    static let lightBorderButtomColor = UIColor(red: 1, green: 0.917, blue: 0.792, alpha: 0.7)
+    static let indicatorColor = UIColor(red: 1, green: 0.753, blue: 0.275, alpha: 1)
 }
