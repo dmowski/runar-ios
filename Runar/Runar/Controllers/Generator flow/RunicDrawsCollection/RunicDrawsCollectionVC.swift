4e4e35ca83bb091d6ecf56b055e888ac083321b0
Kengii <kengii9431@gmail.com>
2023-02-22 20:08:48 +0300

    Changed font size generatorViewDescription Added constant Changed NSLayoutConstraint to snapKit

--- Runar/Runar/Controllers/Generator flow/RunicDrawsCollection/RunicDrawsCollectionVC.swift
+++ Runar/Runar/Controllers/Generator flow/RunicDrawsCollection/RunicDrawsCollectionVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 extension String {
     static let layoutTitle = L10n.Layouts.title
@@ -14,55 +15,97 @@ extension String {
 }
 
 class RunicDrawsCollectionVC: UICollectionViewController {
-    
+
+    // MARK: Constants
+    let topAnchorTitleCollectionView: CGFloat = 38
+    let heightTitleCollectionView: CGFloat = 42
+
+    let topAnchorGeneratorView: CGFloat = 32
+    let trailingLeadingAnchorGeneratorView: CGFloat = 16
+    let heightGeneratorView: CGFloat = 120
+
+    let topAnchorGeneratorHeader: CGFloat = 25
+
+    let topBottomAnchorImage: CGFloat = 8
+    let widthImage: CGFloat = 120
+
+    let topGeneratorDescription: CGFloat = 8
+    let trailingGeneratorDescription: CGFloat = 15
+
+    let paddingCollectionView: CGFloat = 16
+    let minimumItemSpasingCollectionView: CGFloat = 16
+    let aspectRatioCollectioView: CGFloat = 183 / 199
+
     private let data = DataBase.runes.map {
         MainCellModel(runeId: $0.id, name: $0.name, image: $0.image)
     }
-    
-    let label: UILabel = {
-        return UILabel.createAmatic(title: .layoutTitle, size: 36, lineHeight: 0.7, height: 42)
+
+    let titleCollectionView: UILabel = {
+        let label = UILabel()
+        let fontSizeTitleCollectionView: CGFloat = 36
+        label.font = .amaticBold(size: fontSizeTitleCollectionView)
+        label.textColor = .yellowPrimaryColor
+        label.contentMode = .center
+        let prgph = NSMutableParagraphStyle()
+        prgph.lineHeightMultiple = 0.79
+        label.attributedText = NSMutableAttributedString(string: .layoutTitle, attributes: [NSAttributedString.Key.paragraphStyle: prgph])
+
+        return label
     }()
-    
+
     let generatorView: UIView = {
         let generatorView = UIView()
-        generatorView.layer.cornerRadius = 8
-        generatorView.layer.borderWidth = 1
-        generatorView.layer.borderColor = UIColor(red: 0.824, green: 0.769, blue: 0.678, alpha: 0.6).cgColor
-        generatorView.layer.backgroundColor = UIColor(red: 0.063, green: 0.063, blue: 0.063, alpha: 0.35).cgColor
+        let cornerRadiusGeneratorView: CGFloat = 8
+        let borderWidthGeneratorView: CGFloat = 1
+        generatorView.layer.cornerRadius = cornerRadiusGeneratorView
+        generatorView.layer.borderWidth = borderWidthGeneratorView
+        generatorView.layer.borderColor = UIColor.accentTextColor.cgColor
+        generatorView.layer.backgroundColor = UIColor.generatorViewBacgroundColor
         generatorView.translatesAutoresizingMaskIntoConstraints = false
         return generatorView
     }()
-    
+
     let generatorImage: UIImageView = {
         let generatorImage = UIImageView(image: Assets.runePattern.image)
         generatorImage.translatesAutoresizingMaskIntoConstraints = false
         generatorImage.contentMode = .scaleAspectFit
         return generatorImage
     }()
-    
+
     let generatorHeader: UILabel = {
-        let generatorHeader = UILabel.createAmatic(title: .layoutGeneratorTitle, size: 24, lineHeight: 0.79, height: 24)
-        generatorHeader.textColor = UIColor(red: 0.882, green: 0.882, blue: 0.882, alpha: 1)
-        return generatorHeader
+        let label = UILabel()
+        let fontSizeTitleGeneratorView: CGFloat = 24
+        label.font = .amaticBold(size: fontSizeTitleGeneratorView)
+        label.textColor = .textColor
+        label.translatesAutoresizingMaskIntoConstraints = false
+        let prgph = NSMutableParagraphStyle()
+        prgph.lineHeightMultiple = 0.79
+        label.attributedText = NSMutableAttributedString(string: .layoutGeneratorTitle, attributes: [NSAttributedString.Key.paragraphStyle: prgph])
+        return label
     }()
-    
-    let generatorDesc: UILabel = {
-        let generatorDesc = UILabel.createAmatic(title: .layoutGeneratorDesc, size: 15, lineHeight: 1.14, height: 50)
-        generatorDesc.font = .systemRegular(size: 15)
-        generatorDesc.textColor = UIColor(red: 0.882, green: 0.882, blue: 0.882, alpha: 1)
-        generatorDesc.textAlignment = .left
-        generatorDesc.contentMode = .scaleToFill
-        generatorDesc.numberOfLines = 0
-        generatorDesc.lineBreakMode = .byWordWrapping
-        return generatorDesc
+
+    let generatorDescription: UILabel = {
+        let label = UILabel()
+        let fontSizeGereatorDescription: CGFloat = 12
+        label.font = .systemRegular(size: fontSizeGereatorDescription)
+        label.textAlignment = .left
+        label.contentMode = .scaleToFill
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .textColor
+        let prgph = NSMutableParagraphStyle()
+        prgph.lineHeightMultiple = 1.14
+        label.attributedText = NSMutableAttributedString(string: .layoutGeneratorDesc, attributes: [NSAttributedString.Key.paragraphStyle: prgph])
+        return label
     }()
-    
+
     let generatorButton: UIButton = {
         let generatorButton = UIButton()
         generatorButton.translatesAutoresizingMaskIntoConstraints = false
         return generatorButton
     }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -70,10 +113,10 @@ class RunicDrawsCollectionVC: UICollectionViewController {
 
         navigationController?.navigationBar.isHidden = true
         navigationController?.interactivePopGestureRecognizer?.isEnabled = false
-        
+
         configureCollectionView()
         configureConstraints()
-        
+
         // TODO: - No Internet
         //        //NetworkMonitor
         //        if NetworkMonitor.shared.isConnected {
@@ -82,76 +125,73 @@ class RunicDrawsCollectionVC: UICollectionViewController {
         //            showAllert()
         //        }
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.tabBarController?.tabBar.isHidden = false
     }
-    
+
     @objc private func updateUI(_ notification: NSNotification) {
         self.collectionView.reloadData()
     }
 
     private func configureCollectionView() {
-        
+
         let mainImageView: UIImageView = UIImageView(image: Assets.Background.main.image)
         mainImageView.contentMode = .scaleAspectFill
         collectionView.backgroundView = mainImageView
         collectionView.delaysContentTouches = false
         collectionView.register(MainCell.self, forCellWithReuseIdentifier: MainCell.reuseIdentifier)
     }
-    
+
     private func configureConstraints() {
-        
+
         collectionView.addSubview(generatorView)
-        NSLayoutConstraint.activate([
-            generatorView.topAnchor.constraint(equalTo: collectionView.topAnchor, constant: 8),
-            generatorView.leftAnchor.constraint(equalTo: collectionView.leftAnchor, constant: 16),
-            generatorView.rightAnchor.constraint(equalTo: collectionView.rightAnchor, constant: 16),
-            generatorView.centerXAnchor.constraint(equalTo: collectionView.centerXAnchor),
-            generatorView.heightAnchor.constraint(equalToConstant: 120)
-        ])
-        
+        generatorView.snp.makeConstraints { make in
+            make.top.equalTo(collectionView.snp.top).offset(topAnchorGeneratorView)
+            make.trailing.leading.equalTo(collectionView).inset(trailingLeadingAnchorGeneratorView)
+            make.height.equalTo(heightGeneratorView)
+            make.centerX.equalTo(collectionView)
+        }
+
         collectionView.addSubview(generatorButton)
-        
         generatorButton.addTarget(self, action: #selector(self.goToGeneratorTab), for: .touchUpInside)
-        NSLayoutConstraint.activate([
-            generatorButton.topAnchor.constraint(equalTo: generatorView.topAnchor),
-            generatorButton.leftAnchor.constraint(equalTo: generatorView.leftAnchor),
-            generatorButton.rightAnchor.constraint(equalTo: generatorView.rightAnchor),
-            generatorButton.bottomAnchor.constraint(equalTo: generatorView.bottomAnchor)
-        ])
-        
+        generatorButton.snp.makeConstraints { make in
+            make.top.equalTo(generatorView.snp.top)
+            make.trailing.leading.equalTo(generatorView)
+            make.bottom.equalTo(generatorView.snp.bottom)
+        }
+
         generatorView.addSubview(generatorImage)
-        NSLayoutConstraint.activate([
-            generatorImage.topAnchor.constraint(equalTo: generatorView.topAnchor, constant: 8),
-            generatorImage.leftAnchor.constraint(equalTo: generatorView.leftAnchor),
-            generatorImage.bottomAnchor.constraint(equalTo: generatorView.bottomAnchor, constant: -8),
-            generatorImage.widthAnchor.constraint(equalToConstant: 120)
-        ])
-        
+        generatorImage.snp.makeConstraints { make in
+            make.top.equalTo(generatorView.snp.top).offset(topBottomAnchorImage)
+            make.leading.equalTo(generatorView.snp.leading)
+            make.bottom.equalTo(generatorView.snp.bottom).inset(topBottomAnchorImage)
+            make.width.equalTo(widthImage)
+        }
+
         generatorView.addSubview(generatorHeader)
-        NSLayoutConstraint.activate([
-            generatorHeader.topAnchor.constraint(equalTo: generatorView.topAnchor, constant: 25),
-            generatorHeader.leftAnchor.constraint(equalTo: generatorImage.rightAnchor),
-            generatorHeader.rightAnchor.constraint(equalTo: generatorView.rightAnchor)
-        ])
-        
-        generatorView.addSubview(generatorDesc)
-        NSLayoutConstraint.activate([
-            generatorDesc.topAnchor.constraint(equalTo: generatorView.topAnchor, constant: 50),
-            generatorDesc.leftAnchor.constraint(equalTo: generatorImage.rightAnchor),
-            generatorDesc.rightAnchor.constraint(equalTo: generatorView.rightAnchor, constant: -8)
-        ])
-        
-        collectionView.addSubview(label)
-        NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalTo: generatorView.bottomAnchor, constant: 38),
-            label.centerXAnchor.constraint(equalTo: collectionView.centerXAnchor),
-            label.heightAnchor.constraint(equalToConstant: 42)
-        ])
+        generatorHeader.snp.makeConstraints { make in
+            make.top.equalTo(generatorView.snp.top).offset(topAnchorGeneratorHeader)
+            make.leading.equalTo(generatorImage.snp.trailing)
+            make.trailing.equalTo(generatorView.snp.trailing)
+        }
+
+        generatorView.addSubview(generatorDescription)
+        generatorDescription.snp.makeConstraints { make in
+            make.top.equalTo(generatorHeader.snp.bottom).offset(topGeneratorDescription)
+            make.leading.equalTo(generatorImage.snp.trailing)
+            make.trailing.equalTo(generatorView.snp.trailing).inset(trailingGeneratorDescription)
+        }
+
+        collectionView.addSubview(titleCollectionView)
+        titleCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(generatorView.snp.bottom).offset(topAnchorTitleCollectionView)
+            make.height.equalTo(heightTitleCollectionView)
+            make.centerX.equalToSuperview()
+        }
     }
-    
+
     @IBAction private func goToGeneratorTab() {
         self.tabBarController?.selectedIndex = 2
     }
@@ -172,18 +212,18 @@ extension RunicDrawsCollectionVC: UICollectionViewDelegateFlowLayout {
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
-    
+
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return data.count
     }
-    
+
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MainCell.reuseIdentifier, for: indexPath) as! MainCell
         cell.update(with: data[safe: indexPath.row])
 
         return cell
     }
-    
+
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
 
         guard let runeDescription = DataBase.runes.first(where: {
@@ -191,31 +231,31 @@ extension RunicDrawsCollectionVC: UICollectionViewDelegateFlowLayout {
         }) else { return }
 
         tapRunicDrawCell(runeDescription: runeDescription)
-        
+
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width = view.bounds.width
-        let padding: CGFloat = 16
-        let minimumItemSpasing: CGFloat = 16
+        let padding: CGFloat = paddingCollectionView
+        let minimumItemSpasing: CGFloat = minimumItemSpasingCollectionView
         let availableWidth = width - (padding * 2) - minimumItemSpasing
         let itemWidth = availableWidth / 2
-        let aspectRatio: CGFloat = 183 / 199
+        let aspectRatio: CGFloat = aspectRatioCollectioView
         return CGSize(width: itemWidth, height: itemWidth / aspectRatio)
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 230, left: 16, bottom: 16, right: 16)
+        return UIEdgeInsets(top: 250, left: 16, bottom: 16, right: 16)
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 16
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 16
     }
-    
+
     private func tapRunicDrawCell(runeDescription: RuneDescription) {
         if LocalStorage.pull(forKey: runeDescription.name) == true {
             let viewModel = AlignmentVM(runeDescription: runeDescription)
@@ -251,3 +291,9 @@ private extension RuneDescription {
         }
     }
 }
+
+private extension UIColor {
+    static let generatorViewBacgroundColor = UIColor(red: 0.063, green: 0.063, blue: 0.063, alpha: 0.35).cgColor
+    static let textColor = UIColor(red: 0.882, green: 0.882, blue: 0.882, alpha: 1)
+}
+
