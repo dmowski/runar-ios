632945bba88adcd5c87272750205ab791e8fbf04
Aliaksandr Niakhai <sn.sasza@gmail.com>
2023-03-05 20:10:36 +0100

    RUN-80 Local Notification: fix.

--- Runar/Runar/Services/Notifications/LocalNotificationManager.swift
+++ Runar/Runar/Services/Notifications/LocalNotificationManager.swift
@@ -13,9 +13,9 @@ private extension String {
     static let notificationGeneralIdentifier = "General Local Notification"
 }
 
-private enum TimeOfSendingLocalNotifications: Int {
-    case weekday = 2 // Monday
-    case hour = 11   // 11:00
+struct RunarNotificationInfo {
+    let weekday: Int // Sunday(1), Monday(2)...
+    let hour: Int // 11
 }
 
 protocol LocalNotificationInterface {
@@ -27,18 +27,25 @@ protocol LocalNotificationInterface {
 }
 
 final class LocalNotificationManager: NSObject, LocalNotificationInterface {
-
+    
+    // MARK: - private properties
     private let notificationCenter = UNUserNotificationCenter.current()
-
+    private let maxMonthWeeksNumber = 5
+    
+    // MARK: - internal properties
+    let weeklyNotification = RunarNotificationInfo(weekday: 2, hour: 11)
+    
+    // MARK: - private methods
     /// If the user logs into the app on the day of the notification call before the time of the call, the notification on that day will be deleted
     private func checkTheDayAndTime() {
-        let dateComponents = Calendar.current.dateComponents([.weekday, .hour, .weekOfMonth], from: Date())
-        let weekdaySetPoint = TimeOfSendingLocalNotifications.weekday.rawValue
-        let hourSetPoint = TimeOfSendingLocalNotifications.hour.rawValue
+        let dateComponents = Calendar.current.dateComponents([.weekday, .hour, .weekOfMonth, .minute], from: Date())
+        let weekdaySetPoint = weeklyNotification.weekday
+        let hourSetPoint = weeklyNotification.hour
 
         guard let weekday = dateComponents.weekday,
               let hour = dateComponents.hour,
-              let week = dateComponents.weekOfMonth else { return }
+              let week = dateComponents.weekOfMonth
+        else { return }
 
         if weekday == weekdaySetPoint && hour < hourSetPoint {
             var identifier: String = .notificationGeneralIdentifier
@@ -46,20 +53,25 @@ final class LocalNotificationManager: NSObject, LocalNotificationInterface {
             notificationCenter.removePendingNotificationRequests(withIdentifiers: [identifier])
         }
     }
-
+    
     /// Request permission from the user to receive notifications
     private func requestAuthorization() {
-        notificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { [weak self] granted, error in
-            guard let self = self else { return }
-
-            if granted {
-                self.addRequest()
-            } else {
-                print("Local Notification: User has declined notifications")
+        notificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { [weak self] isGranted, error in
+            guard let strongSelf = self else { return }
+            
+            guard error == nil else {
+                print(error?.localizedDescription ?? "Authorization denied")
+                return
             }
+            
+            guard isGranted else {
+                print("Local Notification: User has declined notifications")
+                return
+            }
+            strongSelf.addRequest()
         }
     }
-
+    
     /// The function of generating sending notifications
     private func addRequest() {
         // Notification Content
@@ -68,48 +80,52 @@ final class LocalNotificationManager: NSObject, LocalNotificationInterface {
         content.body = .notificationGeneralContentBody
         content.sound = UNNotificationSound.default
         content.badge = 1
-
+        
         // Configuration the recurring date
         var dateComponents = DateComponents()
         dateComponents.calendar = Calendar.current
-        dateComponents.weekday = TimeOfSendingLocalNotifications.weekday.rawValue
-        dateComponents.hour = TimeOfSendingLocalNotifications.hour.rawValue
-
-        for week in 1...5 {
+        dateComponents.weekday = weeklyNotification.weekday
+        dateComponents.hour = weeklyNotification.hour
+        
+        let weeksRange = 1...maxMonthWeeksNumber
+        
+        weeksRange.forEach {
             var identifier: String = .notificationGeneralIdentifier
-            identifier += "_Week_\(week)"
-            dateComponents.weekOfMonth = week
-
+            identifier += "_Week_\($0)"
+            dateComponents.weekOfMonth = $0
+            
             // Creating the trigger as a repeating event
             let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
-
+            
             // Creating the notification request
             let request = UNNotificationRequest(identifier: identifier,
                                                 content: content,
                                                 trigger: trigger)
-
-            notificationCenter.add(request) { error in
-                if let error = error {
-                    print("Local Notification: Error \(error.localizedDescription)")
+            
+            notificationCenter.add(request) { [weak self] error in
+                guard let _ = self, let error = error else {
+                    return
                 }
+                print("Local Notification error: \(error.localizedDescription)")
             }
         }
-
+        
         checkTheDayAndTime()
     }
-
+    
+    // MARK: - internal methods
     func addNotification() {
         notificationCenter.getNotificationSettings { [weak self] settings in
-            guard let self = self else { return }
-
-            if settings.authorizationStatus == .authorized {
-                self.addRequest()
-            } else {
-                self.requestAuthorization()
+            guard let strongSelf = self else { return }
+            
+            guard settings.authorizationStatus == .authorized else {
+                strongSelf.requestAuthorization()
+                return
             }
+            strongSelf.addRequest()
         }
     }
-
+    
     func removeAllNotifications() {
         notificationCenter.removeAllDeliveredNotifications()
         notificationCenter.removeAllPendingNotificationRequests()
